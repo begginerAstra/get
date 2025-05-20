6fb2c643ccbe9f60291b1734b331036e2c6900b0
begginerAstra <euandersonguedes@gmail.com>
2025-05-20 12:20:19 -0300

    Update clientID.lua

--- clientID.lua
+++ clientID.lua
@@ -5,7 +5,7 @@ print("Seu ClientID é: " .. clientID)
 
 if setclipboard then
     setclipboard(clientID)
-    print(":✅: ClientID copiado para a área de transferência!")
+    print("✅ ClientID copiado para a área de transferência!")
 else
-    warn(":❌: Este executor não suporta 'setclipboard'")
+    warn("❌ Este executor não suporta 'setclipboard'")
 end
